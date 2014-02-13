369f60f570f6891697b8fd6e061b47ad983039ef
lansdell <lansdell@uw.edu>
2014-02-13 11:58:14 -0800

    First! Also, created startup.m for paths

---
+++ matlab/startup.m
@@ -0,0 +1,13 @@
+addpath('./data', './functions');
+addpath_recurse('./worksheets');
+
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Arial')
+set(0,'DefaultAxesFontSize', 8)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Arial')
+set(0,'DefaultTextFontSize', 8)
+
+% Change default line width
+set(0,'DefaultLineLineWidth',1.5)
