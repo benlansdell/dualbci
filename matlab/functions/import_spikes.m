6a9e17622d0a0edf411b0a1f2c6e909ab36e2493
lansdell <lansdell@uw.edu>
2014-02-20 13:15:16 -0800

    Fixed return value problem for import_spikes

--- matlab/functions/import_spikes.m
+++ matlab/functions/import_spikes.m
@@ -1,14 +1,14 @@
-function trial = import_spikes(trial)
+function trial_out = import_spikes(trial_in)
         %import_spikes       Imports spike information from .nev file corresponding to trial loaded from import_trials
         %
         % Usage:
-        %                       import_spikes(trial)
+        %                       import_spikes(trial_in)
         %
         % Input:
-        %                       trial = trial structure from import_trials
+        %                       trial_in = trial structure from import_trials
 	%
 	% Output:
-	%			trial = trial structure from import_trials with nevspikes appended:
+	%			trial_out = trial structure from import_trials with nevspikes appended:
 	%				nevspikes is nE*nT array listing spike activity for each electrode
 	%				in array of nE electrodes, at each time step, sampled at 60Hz, of nT timesteps
         %
@@ -16,21 +16,22 @@ function trial = import_spikes(trial)
         %                       trials = import_trials('Spanky_2013-01-17-1325.mat');
 	%			trial = import_spikes(trials(117));
 
-	NEV=openNEV(['./data/' trial.nevfile]);
+	NEV=openNEV(['./data/' trial_in.nevfile]);
 
 	nevsamplerate = NEV.MetaTags.TimeRes;
 	%Find all spikes that occur within the 1/60s timebin
 	labviewsamplerate = 60;
 	nE = length(NEV.MetaTags.ChannelID);
 	
-	spiketimes = single(NEV.Data.Spikes.TimeStamp)/nevsamplerate + trial.offset;
-	nT = floor(trial.duration*labviewsamplerate)+1;
-	trial.nevspikes = zeros(nE, nT);
+	spiketimes = single(NEV.Data.Spikes.TimeStamp)/nevsamplerate + trial_in.offset;
+	nT = floor(trial_in.duration*labviewsamplerate)+1;
+	trial_in.nevspikes = zeros(nE, nT);
 	for i=1:length(spiketimes)
-		if (spiketimes(i) > trial.starttime) & (spiketimes(i) < trial.endtime)
-			T = floor((spiketimes(i)-trial.starttime)*labviewsamplerate)+1;
-			E = NEV.Data.Spikes.Electrode(i)
-			trial.nevspikes(E,T) = trial.nevspikes(E,T) + 1;
+		if (spiketimes(i) > trial_in.starttime) & (spiketimes(i) < trial_in.endtime)
+			T = floor((spiketimes(i)-trial_in.starttime)*labviewsamplerate)+1;
+			E = NEV.Data.Spikes.Electrode(i);
+			trial_in.nevspikes(E,T) = trial_in.nevspikes(E,T) + 1;
 		end
 	end
+	trial_out = trial_in;
 end
