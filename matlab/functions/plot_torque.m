a74c849751811a987449bbec92f3ab532ea5b275
Ben Lansdell <ben.lansdell@gmail.com>
2014-05-06 13:07:04 -0700

    Added code to compare nsx torque vs labview torque

--- matlab/functions/plot_torque.m
+++ matlab/functions/plot_torque.m
@@ -25,13 +25,33 @@ function plot_torque(trial, fn)
 	end
 
 	%Plot torque between labview and ns3 file
+        %Torque in .nsx file
 	plot(trial.times, trial.ns3data(138,:), trial.times, trial.ns3data(139,:));
 	xlabel('time (s)');
 	saveplot(gcf, [fn '.ns3']);
+        %Torque in .mat file
 	plot(trial.times, trial.torque(:,1), trial.times, trial.torque(:,2));
 	xlabel('time (s)')
 	saveplot(gcf, [fn '.labview']);
+        %Cursor position
 	plot(trial.times, trial.cursor(:,1), trial.times, trial.cursor(:,2));
 	xlabel('time (s)')
 	saveplot(gcf, [fn '.cursor']);
+        %Plot them against one another:
+        %nsx torque x vs mat torque x
+        plot(trial.ns3data(138,:), trial.torque(:,1));
+        xlabel('torque axis 1 ns3 file');
+        ylabel('torque axis 1 mat file');
+        saveplot(gcf, [fn '.ns3_vs_mat']);
+        %nsx torque x vs cursor x
+        plot(trial.ns3data(138,:), trial.cursor(:,1));
+        xlabel('torque axis 1 ns3 file');
+        ylabel('cursor position x');
+        saveplot(gcf, [fn '.ns3_vs_cur']);
+        %nsx torque x vs mat torque x
+        %plot(trial.ns3data(138,:), trial.torque(:,1));
+        %xlabel('torque axis 1 ns3 file');
+        %saveplot(gcf, [fn '.ns3_vs_mat']);
+        
+
 end
