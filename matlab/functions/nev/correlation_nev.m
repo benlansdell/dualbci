f88c8c64b7745096878803da457209e2c4bdbac5
lansdell <lansdell@uw.edu>
2014-05-06 15:45:09 -0700

    added conditional spike density plots

--- matlab/functions/nev/correlation_nev.m
+++ matlab/functions/nev/correlation_nev.m
@@ -10,9 +10,9 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	%			nevfile = file to process
 	%			fn_out = base name for output plots
 	%			threshold = (optional, default = 5) threshold firing rate below which unit is ignored
-	%			binsize = (optional, default = 0.01) time bin size for spikes (torque resmapled to this rate0)
+	%			binsize = (optional, default = 0.002) time bin size for spikes (torque resmapled to this rate0)
 	%			sigma_fr = (optional, default = 0) width of gaussian filter to apply to spikes for firing rate. If 0 then no filter applied
-	%			sigma_trq = (optional, default = 0.5) width of gaussian filter to apply to torque. If 0 then no filter applied
+	%			sigma_trq = (optional, default = 0.25) width of gaussian filter to apply to torque. If 0 then no filter applied
 	%				Note: both sigmas are in units of seconds, and then are scaled according to binsize
 	%		
 	%		Output:
@@ -22,17 +22,17 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	%		Test code:
 	%			nevfile = './testdata/20130117SpankyUtah001.nev';
 	%			threshold = 5;
-	%			binsize = 0.01;
+	%			binsize = 0.002;
 	%			sigma_fr = 0; 
-	%			sigma_trq = 0.5;
-	%			fn_out = './worksheets/preprocessing/corr_20130117SpankyUtah001';
+	%			sigma_trq = 0.25;
+	%			fn_out = './worksheets/tuning/crosscorr/20130117SpankyUtah001';
 	%			correlation_nev(nevfile, fn_out, threshold, binsize, sigma_fr, sigma_trq);
 	
 	%Optional arguments
 	if (nargin < 3)	threshold = 5; end
-	if (nargin < 4) binsize = 0.01; end
+	if (nargin < 4) binsize = 0.002; end
 	if (nargin < 5) sigma_fr = 0; end
-	if (nargin < 6) sigma_trq = 0.5; end
+	if (nargin < 6) sigma_trq = 0.25; end
 	%Set this to above 0 if want debug info/plots
 	verbosity = 1;
 	%Total number of possible units recorded from
@@ -40,7 +40,9 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	nunits = 5; 
 	nU = nE*nunits;
 	%Offset to apply
-	offset = 0;
+	%offset = 0;
+	%Put firing rate ahead of torque
+	offset = -0.150;
 	%Size of gaussian filter to apply
 	samplerate = 1/binsize;
 	%Max lag for correlations
@@ -52,7 +54,6 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	ns3file = [nevfile(1:end-3) 'ns3'];
 
 	%Filters
-    x = linspace(-sz/2, sz/2, sz);
     if sigma_fr > 0
     	sigma_fr = sigma_fr*samplerate;
    		sz = sigma_fr*3;
@@ -135,17 +136,22 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	    end
     end
 	%Resample at rate of binsize
-	torque = resample(nsxtorque', samplerate, nsxsamplerate);		
+	torque = resample(nsxtorque', samplerate, nsxsamplerate);	
+    plot(torque(1:(5*samplerate),1), torque(1:(5*samplerate),2));	
 	%Check they're the same length, and trim
 	nsamp = min(size(torque,1), size(rates,1));
 	torque=torque(1:nsamp,:);
 	rates = rates(1:nsamp,:);
 	%Apply offset to data
 	delaysamples = round(offset*samplerate);
+	%If offset is positive, then firing rate proceeds torque
 	if (delaysamples > 0)
+    	binnedspikes = binnedspikes(1+delaysamples:end,:);
     	rates = rates(1+delaysamples:end,:);
     	torque = torque(1:end-delaysamples,:);
+    %If offset is negative then firing rate precedes torque
 	elseif (delaysamples < 0)
+	    binnedspikes = binnedspikes(1:end+delaysamples,:);
 	    rates = rates(1:end+delaysamples,:);
     	torque = torque(1-delaysamples:end,:);
     end
@@ -155,38 +161,53 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	dtorquex = [diff(torque(:,1)); 0]; dtorquey = [diff(torque(:,2)); 0];
 	ddtorquex = [diff(dtorquex); 0]; ddtorquey = [diff(dtorquey); 0];
-	dtorquex(1) = 0; dtorquey(1) = 0;
-	ddtorquex(1) = 0; ddtorquey(1) = 0;
+	dtorquex(1:5) = 0; dtorquey(1:5) = 0;
+	ddtorquex(1:5) = 0; ddtorquey(1:5) = 0;
+	figure 
+	subplot(2,2,1)
     [ctrs1,ctrs2,nc, priorF] = smoothhist2D([torque(:,1), torque(:,2)], 5, [100, 100], 0.05);
-    [ctrs1,ctrs2,nc, priorFd] = smoothhist2D([dtorquex, dtorquey], 5, [100, 100], 0.05);
-    [ctrs1,ctrs2,nc, priorFdd] = smoothhist2D([ddtorquex, ddtorquey], 5, [100, 100], 0.05);
+    title('Torque position density')
+	subplot(2,2,2)
+    [ctrs1d,ctrs2d,ncd, priorFd] = smoothhist2D([dtorquex, dtorquey], 5, [100, 100], 0.05);
+    title('Torque velocity density')
+	subplot(2,2,3)
+    [ctrs1dd,ctrs2dd,ncdd, priorFdd] = smoothhist2D([ddtorquex, ddtorquey], 5, [100, 100], 0.05);
+    title('Torque accel density')
+    saveplot(gcf, [fn_out '_torque_density.eps'], 'eps', [2 6]);
     for i=1:nU
     	spikeidx = find(binnedspikes(:,i)>0);
     	figure
     	%Position
     	subplot(2,2,1)
-     	[ctrs1,ctrs2,nc, relF] = smoothhist2D([torque(spikeidx,1), torque(spikeidx,2)], 5, [100, 100], 0.05);
+     	[ctrs1c,ctrs2c,nc, relF] = smoothhist2D([torque(spikeidx,1), torque(spikeidx,2)], 5, [100, 100], 0.05);
      	normF = relF./priorF;
-     	image(ctrs1,ctrs2,floor(nc.*normF) + 1);
+     	normFscaled = normF/max(max(normF(:)));
+     	image(ctrs1,ctrs2,floor(nc.*normFscaled) + 1);
+%     	image(ctrs1,ctrs2,floor(nc.*normF) + 1);
     	title(unitnames{i})
-    	xlim([-0.5 0.5])
-    	ylim([-0.5 0.5])
+%    	xlim([-0.5 0.5])
+%    	ylim([-0.5 0.5])
     	%Velocity
     	subplot(2,2,2)
-     	[ctrs1,ctrs2,nc, relF] = smoothhist2D([dtorquex(spikeidx), dtorquey(spikeidx)], 5, [100, 100], 0.05);
-     	normFd = relF./priorFd;
-     	image(ctrs1,ctrs2,floor(nc.*normFd) + 1);
-	   	xlim([-0.02 0.02])
-    	ylim([-0.02 0.02])
+     	[ctrs1cd,ctrs2cd,nc, relF] = smoothhist2D([dtorquex(spikeidx), dtorquey(spikeidx)], 5, [100, 100], 0.05);
+     	normF = relF./priorFd;
+     	normFscaled = normF/max(max(normF(:)));
+     	image(ctrs1d,ctrs2d,floor(nc.*normFscaled) + 1);
+%     	image(ctrs1d,ctrs2d,floor(nc.*normF) + 1);
+	   	%xlim([-0.02 0.02])
+    	%ylim([-0.02 0.02])
     	%Accel
     	subplot(2,2,3)
-     	[ctrs1,ctrs2,nc, relF] = smoothhist2D([ddtorquex(spikeidx), ddtorquey(spikeidx)], 5, [100, 100], 0.05);
+     	[ctrs1cdd,ctrs2cdd,nc, relF] = smoothhist2D([ddtorquex(spikeidx), ddtorquey(spikeidx)], 5, [100, 100], 0.05);
      	normF = relF./priorFdd;
-     	image(ctrs1,ctrs2,floor(nc.*normF) + 1);
+     	normFscaled = normF/max(max(normF(:)));
+		image(ctrs1dd,ctrs2dd,floor(nc.*normFscaled) + 1);
+%		image(ctrs1dd,ctrs2dd,floor(nc.*normF) + 1);
     	title(unitnames{i})
-    	xlim([-0.5 0.5])
-    	ylim([-0.5 0.5])
-    	pause
+    	%xlim([-0.02 0.02])
+    	%ylim([-0.02 0.02])
+   		saveplot(gcf, [fn_out '_spikedensity_unit_' unitnames{i} '.eps'], 'eps', [5 5]);
+%    	pause
     end
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -215,7 +236,7 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
    	plot(tt,autotorqueRU);
    	xlim([-maxpeak*2 maxpeak*2])
    	title('Auto-corr torque RU');
-	saveplot(gcf, [fn_out '_auto-torque.eps'], 'eps', [2 4]);
+	saveplot(gcf, [fn_out '_auto-torque.eps'], 'eps', [4 2]);
 
     for i=1:nU
     	%Compute cross correlation
@@ -273,9 +294,74 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 		xlim([-maxpeak maxpeak])
 
 		saveplot(gcf, [fn_out '_unit_' unitnames{i} '_cross_maxscore_' num2str(max(scoreFE(i), scoreRU(i))) '.eps'], 'eps', [6 6]);
-		pause
+%		pause
     end
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %Cross-correlation with torque velocity and firing rate%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    peakDRU = zeros(1,nU);
+    peakDFE = zeros(1,nU);
+    lagDRU = zeros(1,nU);
+    lagDFE = zeros(1,nU);
+    stdDRU = zeros(1,nU);
+    stdDFE = zeros(1,nU);
+    scoreDRU = zeros(1,nU);
+    scoreDFE = zeros(1,nU);
+
+    for i=1:nU
+    	%Compute cross correlation
+	    covDFE = xcov(rates(:,i), dtorquex,samplerate*maxlag,'unbiased');
+    	% normalize against spikes auto-covariance
+    	covDFE = covDFE / sqrt(xcov(rates(:,i),0));
+    	covDFE = covDFE / sqrt(xcov(dtorquex,0));
+    	peakDFE(i) = covDFE(abs(covDFE) == max(abs(covDFE(peakrange))));
+	    stdDFE(i) = std(covDFE);
+    	lagDFE(i) = tt((covDFE == peakDFE(i)) | (covDFE == -peakDFE(i)));
+    	avg = mean(covDFE);
+    	peakDFE(i) = peakDFE(i) - avg;
+    	scoreDFE(i) = peakDFE(i)/stdDFE(i);
+    	%Plot cross correlation
+    	figure
+    	subplot(3,2,1);
+    	plot(tt,covDFE,[-maxlag maxlag],[avg avg],...
+               [lagDFE(i) lagDFE(i)],[avg avg+peakDFE(i)],...
+                  [-maxlag maxlag],avg+[stdDFE(i) stdDFE(i)]*sign(peakDFE(i)));
+		title(['FE Unit: ' unitnames{i} ' Score: ' num2str(scoreDFE(i))])
+		xlim([-maxlag maxlag])
+    	subplot(3,2,3);
+    	plot(tt,covDFE,[-maxlag maxlag],[avg avg],...
+               [lagDFE(i) lagDFE(i)],[avg avg+peakDFE(i)],...
+                  [-maxlag maxlag],avg+[stdDFE(i) stdDFE(i)]*sign(peakDFE(i)));
+		xlim([-maxpeak maxpeak])
+
+	    covDRU = xcov(rates(:,i), dtorquey,samplerate*maxlag,'unbiased');
+    	% normalize against spikes auto-covariance
+    	covDRU = covDRU / sqrt(xcov(rates(:,i),0));
+    	covDRU = covDRU / sqrt(xcov(dtorquey,0));
+    	peakDRU(i) = covDRU(abs(covDRU) == max(abs(covDRU(peakrange))));
+    	stdDRU(i) = std(covDRU);
+    	lagDRU(i) = tt((covDRU == peakDRU(i)) | (covDRU == -peakDRU(i)));
+    	avg = mean(covDRU);
+    	peakDRU(i) = peakDRU(i) - avg;
+    	scoreDRU(i) = peakDRU(i)/stdRU(i);
+    	%Plot cross- and auto-correlations
+    	subplot(3,2,2);
+    	plot(tt,covDRU,[-maxlag maxlag],[avg avg],...
+               [lagDRU(i) lagDRU(i)],[avg avg+peakDRU(i)],...
+                  [-maxlag maxlag],avg+[stdDRU(i) stdDRU(i)]*sign(peakDRU(i)));
+		title(['RU Score: ' num2str(scoreDRU(i))])
+		xlim([-maxlag maxlag])
+    	subplot(3,2,4);
+    	plot(tt,covDRU,[-maxlag maxlag],[avg avg],...
+               [lagDRU(i) lagDRU(i)],[avg avg+peakDRU(i)],...
+                  [-maxlag maxlag],avg+[stdDRU(i) stdDRU(i)]*sign(peakDRU(i)));
+		xlim([-maxpeak maxpeak])
+
+		saveplot(gcf, [fn_out '_unit_' unitnames{i} '_cross_vel_maxscore_' num2str(max(scoreDFE(i), scoreDRU(i))) '.eps'], 'eps', [6 6]);
+%		pause
+    end
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	%Heat map of tau and xcov for all units%
@@ -350,10 +436,18 @@ function [scoreFE, scoreRU] = correlation_nev(nevfile, fn_out, threshold, binsiz
 	legend('FE', 'RU')
 	saveplot(gcf, [fn_out '_lag_vs_score.eps'])
 
+	%Plot lag vs scores
+	figure
+	plot(abs(scoreDFE), lagDFE, '.b', abs(scoreDRU), lagDRU, '.r')
+	xlabel('|score|')
+	ylabel('lag (s)')
+	legend('FE', 'RU')
+	saveplot(gcf, [fn_out '_vel_lag_vs_score.eps'])
+
 	%figure
-	%plot([0,scoreFE], [0, scoreRU], '.')
-	%xlabel('FE')
-	%ylabel('RU')
-	%saveplot(gcf, [fn_out '_pv.eps'])
+	%plot(zeros(nU,2), [scoreFE'; scoreRU'], '.b')
+	%xlabel('score FE')
+	%ylabel('score RU')
+	%saveplot(gcf, [fn_out '_popvec.eps'])
 
 end
