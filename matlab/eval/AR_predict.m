93b803980be142780a706fef4c1ec47bfe6905c1
Ben Lansdell <lansdell@uw.edu>
2014-09-30 14:44:42 -0700

    Added autogressive model

--- matlab/eval/AR_predict.m
+++ matlab/eval/AR_predict.m
@@ -18,12 +18,14 @@ function y = AR_predict(data, F, mu, fn_out)
 	%	fn_out = './worksheets/diagnostics/AR_test.eps';
 	%	pre = load('./testdata/test_preprocess_spline_short.mat');
 	%	order = 1;
+	%	horizon = 1;
 	%	data = pre.processed.torque;
-	%	[F, Q, mu] = fit_AR(data, order);
+	%	[F, Q, mu] = fit_AR(data, order, horizon);
 	%	AR_predict(data, F, mu, fn_out);
 
 	clf;
-	order = size(F,1)/2;
+	horizon = size(F,1);
+	order = size(F,2)/2;
 	for idx = 1:2
 		subplot(2,1,idx)
 		Y = data(order+1:end,idx);
@@ -36,7 +38,7 @@ function y = AR_predict(data, F, mu, fn_out)
 		X_FE = toeplitz(c,r);
 		%Fit a constant term
 		X = [ones(size(X_RU,1),1), X_RU, X_FE];
-		beta_hat = [mu(idx); F(1:2*order,idx)];
+		beta_hat = [mu(idx); F(1,1:2*order,idx)];
 		Y_hat = X*beta_hat;
 		plot(Y);
 		hold on
--- matlab/fitting/fit_AR.m
+++ matlab/fitting/fit_AR.m
@@ -1,53 +1,39 @@
-function [F, Q, mu] = fit_AR(data, order)
-	%Fit an auto-regressive model to torque data of a given order using least squares. That is, fits a model of the form:
-	%
-	%	[x_2 y_2] ~ [mu_1, mu_2] + [x_{1}, x_{0}, y_{1}, y_{0},...]x[F_11 F_21] + [\epsilon_i^1, \epsilon_i^2]
-	%	[x_3 y_3]				   [x_{2}, x_{1}, y_{2}, y_{1},...] [F_12 F_22]
-	%	[...    ]				   [...                           ] [F_13 F_23]
-	%											  	                [...      ]
+function [F, Q, mu] = fit_AR(data, order, horizon, every)
+	%Fit an auto-regressive model to torque data of a given order using arfit package.
 	%
 	%Usage:
-	%	[F, Q, mu] = fit_AR(data, order)
+	%	[F, Q, mu] = fit_AR(data, order, horizon, every)
 	%     
 	%Input:
 	%	data = [N x 2] matrix where N is the number of data points. Could be cursor position data, 
 	%			or cursor (direction, velocity) data...
 	%	order = (optional, default = 1) Order of AR model to fit
+	%	horizon = (optional, default = 1) Number of steps into the future to predict
+	%	every = (optional, default = 1) Size of the steps to take to predict future trajectories
 	%   
 	%Output:
-	%	F = fitted coefficient matrix
+	%	F = [horizon, order*2, 2] matrix with fit coefficients
 	%	Q = covariance matrix giving errors
 	%	mu = mean data
-	%  
 	%
 	%Test code:
 	%	%Load test preprocessed data
 	%	pre = load('./testdata/test_preprocess_spline_short.mat');
 	%	order = 1;
+	%	every = 1;
 	%	data = pre.processed.torque;
 	%	[F, Q, mu] = fit_AR(data, order);
 
 	if (nargin < 2) order = 1; end
-	F = zeros(order*2, 2);
-	mu = zeros(1, 2);
+	if (nargin < 3) horizon = 1; end
+	if (nargin < 4) every = 1; end
+	F = zeros(horizon, order*2, 2);
+	mu = zeros(horizon, 2);
 	residuals = zeros(size(data,1)-order, size(data,2));
+	pmin = order;
+	pmax = order;
 
-	for idx = 1:2
-		Y = data(order+1:end,idx);
-		%Set up matrices
-		c = data(order:end-1, 1);
-		r = data(order:-1:1,1);
-		X_RU = toeplitz(c,r);
-		c = data(order:end-1,2);
-		r = data(order:-1:1,2);
-		X_FE = toeplitz(c,r);
-		%Fit a constant term
-		X = [ones(size(X_RU,1),1), X_RU, X_FE];
-		%Do the least squares fit:
-		beta_hat = (transpose(X)*X)\(transpose(X)*Y);
-		mu(idx) = beta_hat(1);
-		F(1:2*order,idx) = beta_hat(2:end);
-		residuals(:,idx) = Y - X*beta_hat;
+	for h = 1:horizon
+		[w, A, C] = arfit(data, pmin, pmax);
 	end
 	%Compute the variance/covariance of residuals, Q.
-	Q = cov(residuals(:,1), residuals(:,2));
