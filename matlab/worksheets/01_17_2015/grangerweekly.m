096206bfae34cbc418c2ddd67295e88b101f8248
Ben Lansdell <lansdell@uw.edu>
2015-01-21 14:12:44 -0800

    grangerweekly.m: check if file exists before trying to open

--- matlab/worksheets/01_17_2015/grangerweekly.m
+++ matlab/worksheets/01_17_2015/grangerweekly.m
@@ -1,8 +1,7 @@
 %Granger analysis on weekly dataset
 duration = 360; %at least six minutes
 fn_out = './worksheets/01_17_2015/weeklynevs.mat';
-conds = {'2D Manual Position',
-    'Dual Control'}
+conds = {'2D Manual Position', 'Dual Control'};
 load(fn_out);
 binsize = 1/60;
 threshold = 5;
@@ -22,16 +21,26 @@ for i = 1:length(conds)
 		nevfile = ['./blackrock/' recinfo.nevfile];
 		matfile = ['./labview/' recinfo.matfile];
 		currdate = strrep(recinfo.date, '/', '-');
-		%Preprocess data
-		processed = preprocess_spline_lv(nevfile, matfile, binsize, threshold, offset);		
-		%Combine data to same electrode
-		processed_mua = combine_mua(processed);
-		%Run with position filters_sp_pos_network
-		fn_out = ['./worksheets/01_17_2015/plots/granger_pos_' condition '_' currdate '.eps'];
-		data = filters_sp_pos_network_lv(processed_mua, nK_sp, nK_pos);
-		[GCdev, GCpval, GCsig] = granger(processed, data, fn_out, pval);		
-		%Save the GC matrices for producing GC plots in cytoscape
-		fn_out = ['./worksheets/01_17_2015/grangerresults/' condition '_' currdate '.mat'];
-		save(fn_out, 'GCdev', 'GCpval', 'GCsig', 'processed_mua');
+		if exist(['./worksheets/01_17_2015/grangerresults/' condition '_' currdate '.mat'], 'file')
+			display([condition '_' currdate '.mat exists, continuing to next file.'])
+			continue
+		end
+		if (exist(nevfile, 'file') == 2) & (exist(matfile, 'file') == 2)
+			%Preprocess data
+			processed = preprocess_spline_lv(nevfile, matfile, binsize, threshold, offset);		
+			%Combine data to same electrode
+			processed_mua = combine_mua(processed);
+			%Truncate to 'duration' seconds of recording
+			processed_mua = truncate_recording(processed_mua, duration);
+			%Run with position filters_sp_pos_network
+			fn_out = ['./worksheets/01_17_2015/plots/granger_pos_' condition '_' currdate '.eps'];
+			data = filters_sp_pos_network_lv(processed_mua, nK_sp, nK_pos);
+			[GCdev, GCpval, GCsig] = granger(processed_mua, data, fn_out, pval);		
+			%Save the GC matrices for producing GC plots in cytoscape
+			fn_out = ['./worksheets/01_17_2015/grangerresults/' condition '_' currdate '.mat'];
+			save(fn_out, 'GCdev', 'GCpval', 'GCsig', 'processed_mua');
+		else
+			display(['Missing files: ' nevfile ' or ' matfile '. Continuing to next file.'])
+		end
 	end
 end
