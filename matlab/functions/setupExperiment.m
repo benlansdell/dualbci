333dbce33cebd64e49e9bbeb6a644fb8b68cb70c
Ben Lansdell <lansdell@uw.edu>
2015-02-18 12:44:00 -0800

    Added more settings

--- matlab/functions/setupExperiment.m
+++ matlab/functions/setupExperiment.m
@@ -40,10 +40,59 @@ function settings = setupExperiment(expttype)
             settings.nK_sp = 100;
             settings.nK_pos = 0;
             settings.filters = @(proc) filters_sprc_pos(proc, settings.nK_sp, settings.nK_pos);
+        case 'sprc_pos_lv_def'
+            %Preprocess settings
+            settings.binsize = 0.002;
+            settings.duration = 360; 
+            settings.threshold = 3;
+            settings.offset = 0;
+            settings.const = 'on';
+            settings.process = @(nev, mat) preprocess_spline_lv(nev, mat, settings.binsize, settings.threshold, settings.offset);
+            %Filter settings
+            settings.dt_sp = settings.binsize;
+            settings.dt_pos = 0.2;
+            settings.nK_sp = 100;
+            settings.nK_pos = 5;
+            settings.filters = @(proc) filters_sprc_pos_lv(proc, settings.nK_sp, settings.nK_pos, settings.dt_sp, settings.dt_pos);
+        case 'sprc_pos_target_lv_def'
+            %Preprocess settings
+            settings.binsize = 0.002;
+            settings.duration = 360; 
+            settings.threshold = 3;
+            settings.offset = 0;
+            settings.const = 'on';
+            settings.process = @(nev, mat) preprocess_spline_lv(nev, mat, settings.binsize, settings.threshold, settings.offset);
+            %Filter settings
+            settings.dt_sp = settings.binsize;
+            settings.dt_tar = settings.binsize;
+            settings.dt_pos = 0.2;
+            settings.nK_sp = 100;
+            settings.nK_pos = 5;
+            settings.nK_tar = 1;
+            settings.filters = @(proc) filters_sp_pos_target_lv(proc, settings.nK_sp, settings.nK_pos, settings.nK_tar, settings.dt_sp, settings.dt_pos, settings.dt_tar);
+        case 'sprc_pos_network_lv_def'
+            %Preprocess settings
+            settings.binsize = 0.002;
+            settings.duration = 360; 
+            settings.threshold = 3;
+            settings.offset = 0;
+            settings.const = 'on';
+            settings.process = @(nev, mat) preprocess_spline_lv(nev, mat, settings.binsize, settings.threshold, settings.offset);
+            %Filter settings
+            settings.dt_sp = settings.binsize;
+            settings.dt_pos = 0.2;
+            settings.nK_sp = 100;
+            settings.nK_pos = 5;
+            settings.filters = @(proc) filters_sprc_pos_network_lv(proc, settings.nK_sp, settings.nK_pos, settings.dt_sp, settings.dt_pos);
         otherwise
             error('See help setupExperiment for valid settings names');
     end
     settings.modelname = expttype;
-    settings.matfiledir = '/home/lansdell/fairbanks/labview/Moritz Primate MAT Files 2012-2013/';
-    settings.nevfiledir = '/home/lansdell/fairbanks/blackrock/';
+    if ismac
+        homefolder = '/Users/';
+    else
+        homefolder = '/home/';
+    end
+    settings.matfiledir = [homefolder 'lansdell/fairbanks/labview/Moritz Primate MAT Files 2012-2013/'];
+    settings.nevfiledir = [homefolder 'lansdell/fairbanks/blackrock/'];
 end
