26578686f3024149c2342277d49bd40aadbc3203
Ben Lansdell <lansdell@uw.edu>
2015-03-12 10:29:18 -0700

    02_18_2015: added scripts to run more models on daily dataset

---
+++ matlab/worksheets/02_18_2015/2dmanual_1day_sprc_posvel_lv_run.m
@@ -0,0 +1,5 @@
+matfile_in = './expts/2dmanualpos_1day.mat';
+settings = setupExperiment('sprc_posvel_lv_def');
+exptname = '2dmanualpos_sprc_posvel_lv_def';
+expts = runExperiment(matfile_in, settings, exptname);
+save(['./expts/' exptname '.mat'], 'expts');
