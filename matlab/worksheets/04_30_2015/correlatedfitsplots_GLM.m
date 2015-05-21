7eeb0ce7c860d33d6e322df8290e79949c091a4c
Ben Lansdell <lansdell@uw.edu>
2015-05-21 10:17:26 -0700

    Scripts to make demonstration of estimation of LM and GLM under correlated inputs

--- matlab/worksheets/04_30_2015/correlatedfitsplots_GLM.m
+++ matlab/worksheets/04_30_2015/correlatedfitsplots_GLM.m
@@ -1,7 +1,7 @@
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Sensitivity to small changes in data%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Sensitivity to small changes in data for GLM%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
 beta1 = 0.5; beta2 = 1;
@@ -13,40 +13,43 @@ eps = 0.5;
 x1 = randn(N,1);
 x2 = randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson', 'const', 'off');
 
 %Plot in 3D
 clf
 subplot(1,2,1)
 tt = -3:0.1:3;
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [0.75, 0.75, 1], 'edgecol', 'no');%,...
 alpha(0.5)
 hold on
 scatter3(x1, x2, y, '.b')
 
-%Then repeat with another set of estimates
+%Plot for another set
 x1 = randn(N,1);
 x2 = randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson', 'const', 'off')
 
 %Plot in 3D
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [1, 0.75, 0.75], 'edgecol', 'no');%,...
 alpha(0.5)
 scatter3(x1, x2, y, '.r')
-xlabel('x_1'); ylabel('x_2'); zlabel('y')
 
 subplot(1,2,2)
 %Plot 'energy function'
-energy = @(b1, b2) (y-b1*x1-b2*x2)'*(y-b1*x1-b2*x2);
+tt = -5:0.1:5;
+[X, Y] = meshgrid(tt,tt);
+energy = @(b1, b2) log(sum((y+0.25).*log((y+0.25)./exp(b1*x1+b2*x2))-((y+0.25)-exp(b1*x1+b2*x2))));
 Z = zeros(size(X));
 for i = 1:size(X,1)
 	for j = 1:size(X,2)
@@ -58,8 +61,7 @@ hold on
 xlabel('\beta_1')
 ylabel('\beta_2')
 plot(beta2, beta1, 'r.', 'linewidth', 20)
-%saveplot(gcf, './worksheets/04_30_2015/correlatedfitsplot_indep.eps')
-plot2svg('./worksheets/04_30_2015/correlatedfitsplot_indep.svg')
+plot2svg('./worksheets/04_30_2015/correlatedfitsplot_GLM_indep.svg')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Then do the same for correlated X data...%
@@ -71,45 +73,47 @@ beta1 = 0.5; beta2 = 1;
 %First, without correlations between x's
 N = 100;
 eps = 0.5;
-eps2 = 0.3;
+eps2 = 0.2;
 
 x1 = randn(N,1);
-x2 = x1+eps2*randn(N,1);
+x2 = x1 + eps2*randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson', 'const', 'off')
 
 %Plot in 3D
 clf
 subplot(1,2,1)
 tt = -3:0.1:3;
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [0.75, 0.75, 1], 'edgecol', 'no');%,...
 alpha(0.5)
 hold on
 scatter3(x1, x2, y, '.b')
 
-%Then repeat with another set of estimates
 x1 = randn(N,1);
-x2 = x1+eps2*randn(N,1);
+x2 = x1 + eps2*randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson', 'const', 'off')
 
 %Plot in 3D
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [1, 0.75, 0.75], 'edgecol', 'no');%,...
 alpha(0.5)
 scatter3(x1, x2, y, '.r')
-xlabel('x_1'); ylabel('x_2'); zlabel('y')
 
 subplot(1,2,2)
 %Plot 'energy function'
-energy = @(b1, b2) (y-b1*x1-b2*x2)'*(y-b1*x1-b2*x2);
+tt = -5:0.1:5;
+[X, Y] = meshgrid(tt,tt);
+energy = @(b1, b2) log(sum((y+0.25).*log((y+0.25)./exp(b1*x1+b2*x2))-((y+0.25)-exp(b1*x1+b2*x2))));
 Z = zeros(size(X));
 for i = 1:size(X,1)
 	for j = 1:size(X,2)
@@ -118,11 +122,10 @@ for i = 1:size(X,1)
 end
 pcolor(X, Y, Z);
 hold on
-xlabel('\beta_2')
-ylabel('\beta_1')
-plot(beta1, beta2, 'r.', 'linewidth', 20)
-%saveplot(gcf, './worksheets/04_30_2015/correlatedfitsplot_corr.eps')
-plot2svg('./worksheets/04_30_2015/correlatedfitsplot_corr.svg')
+xlabel('\beta_1')
+ylabel('\beta_2')
+plot(beta2, beta1, 'r.', 'linewidth', 20)
+plot2svg('./worksheets/04_30_2015/correlatedfitsplot_GLM_corr.svg')
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -135,45 +138,47 @@ beta1 = 0.5; beta2 = 1;
 %First, without correlations between x's
 N = 100;
 eps = 0.5;
-eps2 = 0.8;
+eps2 = 0.5;
 
 x1 = randn(N,1);
-x2 = x1+eps2*randn(N,1);
+x2 = x1 + eps2*randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson')
 
 %Plot in 3D
 clf
 subplot(1,2,1)
 tt = -3:0.1:3;
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [0.75, 0.75, 1], 'edgecol', 'no');%,...
 alpha(0.5)
 hold on
 scatter3(x1, x2, y, '.b')
 
-%Then repeat with another set of estimates
 x1 = randn(N,1);
-x2 = x1+eps2*randn(N,1);
+x2 = x1 + eps2*randn(N,1);
 X = [x1, x2];
-y = beta1*x1+beta2*x2 + eps*randn(N,1);
-%Fit for beta1_hat and beta2_hat
-bhat1 = (X'*X)\X'*y;
+mu = exp(beta1*x1+beta2*x2);
+y = poissrnd(mu);
+%Fit for beta1_hat and beta2_hat through IRLS
+bhat1 = glmfit(X, y, 'poisson')
 
 %Plot in 3D
 [X, Y] = meshgrid(tt,tt);
-Z = bhat1(1).*X+bhat1(2).*Y;
+Z = exp(bhat1(1).*X+bhat1(2).*Y);
 S = surf(X,Y,Z, 'facecol', [1, 0.75, 0.75], 'edgecol', 'no');%,...
 alpha(0.5)
 scatter3(x1, x2, y, '.r')
-xlabel('x_1'); ylabel('x_2'); zlabel('y')
 
 subplot(1,2,2)
 %Plot 'energy function'
-energy = @(b1, b2) (y-b1*x1-b2*x2)'*(y-b1*x1-b2*x2);
+tt = -5:0.1:5;
+[X, Y] = meshgrid(tt,tt);
+energy = @(b1, b2) log(sum((y+0.25).*log((y+0.25)./exp(b1*x1+b2*x2))-((y+0.25)-exp(b1*x1+b2*x2))));
 Z = zeros(size(X));
 for i = 1:size(X,1)
 	for j = 1:size(X,2)
@@ -182,8 +187,7 @@ for i = 1:size(X,1)
 end
 pcolor(X, Y, Z);
 hold on
-xlabel('\beta_2')
-ylabel('\beta_1')
-plot(beta1, beta2, 'r.', 'linewidth', 20)
-%saveplot(gcf, './worksheets/04_30_2015/correlatedfitsplot_corr.eps')
-plot2svg('./worksheets/04_30_2015/correlatedfitsplot_intermedcorr.svg')
+xlabel('\beta_1')
+ylabel('\beta_2')
+plot(beta2, beta1, 'r.', 'linewidth', 20)
+plot2svg('./worksheets/04_30_2015/correlatedfitsplot_GLM_intermedcorr.svg')
