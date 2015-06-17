5aed735129f983df484da11961792e71938d6b1b
Ben Lansdell <lansdell@uw.edu>
2015-06-17 00:02:59 -0700

    Run lineartorque model script

---
+++ matlab/worksheets/2015_06_15/lineartuning_fits.m
@@ -0,0 +1,28 @@
+%Script to run simple linear regression on manual control between 2013-09-20 and 2014-01-01
+modelID = 1;
+blackrock = './blackrock/';
+threshold = 5;
+after = '2013-09-20';
+before = '2014-01-01';
+tasktype = 'manual';
+duration = 180;
+
+%Fetch paramcode to load
+conn = database('','root','Fairbanks1!','com.mysql.jdbc.Driver', ...
+	'jdbc:mysql://fairbanks.amath.washington.edu:3306/Spanky');
+paramcode = exec(conn, ['SELECT `description` FROM Models WHERE modelID = ' num2str(modelID)]);
+paramcode = fetch(paramcode);
+paramcode = paramcode.Data{1};
+
+%Fetch files to analyze
+%Fetch all files 
+toprocess = exec(conn, ['SELECT `nev file` FROM Recordings WHERE `nev date` BETWEEN "'...
+ after '" AND "' before '" AND `tasktype` = "' tasktype '" AND `duration` > ' num2str(duration)]);
+toprocess = fetch(toprocess);
+toprocess = toprocess.Data;
+nR = size(toprocess,1);
+for idx = 1:nR
+	nevfile = toprocess{idx, 1};
+	display(['Processing ' nevfile])
+	processLinear(conn, modelID, blackrock, nevfile, paramcode, threshold);
+end
