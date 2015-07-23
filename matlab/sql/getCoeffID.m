1a4e2b4159b3b5d11945848bd941272031a81df9
Ben Lansdell <lansdell@uw.edu>
2015-07-23 14:05:26 -0700

    removed getCoeffID

--- matlab/sql/getCoeffID.m
+++
@@ -1,9 +0,0 @@
-function coeffID = getCoeffID(conn)
-	coeff = exec(conn, ['SELECT MAX(`fitID`) FROM `Fits`']);
-	coeff = fetch(coeff);
-	if isnan(coeff.Data{1}) | strcmp(coeff.Data{1}, 'No data')
-		coeffID = 0;
-	else
-		coeffID = coeff.Data{1}+1;
-	end
-end
