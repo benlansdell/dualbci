78261a3cdfeb370a9e6fcc1b763df9c34094808f
Ben Lansdell <lansdell@uw.edu>
2015-08-05 22:43:15 -0700

    attempt to fix currCommit

--- matlab/functions/currCommit.m
+++ matlab/functions/currCommit.m
@@ -1,3 +1,8 @@
 function comm = currCommit()
 	comm = git('rev-parse --short HEAD');
+	%Filler to stop git() function from including output of next line's 
+	%execution (who knows why???)
+	a = 0;
+	b = 0;
+	c = 0;
 end
