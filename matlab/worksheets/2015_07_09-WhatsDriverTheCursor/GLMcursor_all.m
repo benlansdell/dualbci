92a56d7a3c5216d6c377a7656360b2931aff643d
Ben Lansdell <lansdell@uw.edu>
2015-08-10 16:25:07 -0700

    process GLM cursor scripts

---
+++ matlab/worksheets/2015_07_09-WhatsDriverTheCursor/GLMcursor_all.m
@@ -0,0 +1,46 @@
+%Script to run simple linear regression on manual control between 2013-09-20 and 2014-01-01
+modelID = 18;
+blackrock = './blackrock/';
+labviewpath = './labview/';
+
+%Fetch paramcode to load
+conn = database('','root','Fairbanks1!','com.mysql.jdbc.Driver', ...
+	'jdbc:mysql://fairbanks.amath.washington.edu:3306/spanky_db');
+paramcode = exec(conn, ['SELECT `description` FROM models WHERE modelID = ' num2str(modelID)]);
+paramcode = fetch(paramcode);
+paramcode = paramcode.Data{1};
+
+%Fetch each pair of nev files to run
+conn = database('','root','Fairbanks1!','com.mysql.jdbc.Driver', ...
+	'jdbc:mysql://fairbanks.amath.washington.edu:3306/spanky_db');
+tablename = '`experiment_tuning`';
+toprocess = exec(conn, ['SELECT `1DBCrecording`, `manualrecording`, `manualrecordingafter` FROM ' tablename]);
+toprocess = fetch(toprocess);
+toprocess = toprocess.Data;
+toprocess = reshape(toprocess, [], 1);
+nR = size(toprocess,1);
+eval(paramcode);
+
+for idx = 1:nR
+	nevfile = toprocess{idx};
+	matfile = exec(conn, ['SELECT `labview file` FROM recordings WHERE `nev file` = "' nevfile '"']);
+	matfile = fetch(matfile);
+	matfile = matfile.Data{1};
+	%Figure out which units to process, that haven't already been processed by this model
+	units = exec(conn, ['SELECT u.`unit` FROM units u'...
+		' WHERE u.`nev file` = "' nevfile '" AND u.`firingrate` > ' num2str(threshold)...
+		' AND NOT EXISTS (SELECT * FROM fits f WHERE f.`unit` = u.`unit` AND'...
+		' f.modelID = ' num2str(modelID) ' AND f.`nev file` = u.`nev file`)'	]);
+	units = fetch(units);
+	units = units.Data;
+	display(['Processing ' nevfile])
+	if exist([blackrock nevfile], 'file')
+		if ~strcmp(units, 'No Data')
+			processGLMCursor(conn, modelID, blackrock, labviewpath, nevfile, matfile, paramcode, units);
+		else
+			display('Already processed')
+		end
+	else
+		display('Cannot find file, continuing')
+	end
+end
