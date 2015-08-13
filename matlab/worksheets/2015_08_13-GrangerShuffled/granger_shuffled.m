3d469d8cba4571a34672c2cb51dd8dbb70a292a4
Ben Lansdell <lansdell@uw.edu>
2015-08-13 16:44:31 -0700

    Add processGrangerGLMShuffled

---
+++ matlab/worksheets/2015_08_13-GrangerShuffled/granger_shuffled.m
@@ -0,0 +1,29 @@
+%Script to run simple linear regression on manual control between 2013-09-20 and 2014-01-01
+modelID = 22;
+blackrock = './blackrock/';
+
+%Fetch paramcode to load
+conn = database('','root','Fairbanks1!','com.mysql.jdbc.Driver', ...
+	'jdbc:mysql://fairbanks.amath.washington.edu:3306/spanky_db');
+paramcode = exec(conn, ['SELECT `description` FROM models WHERE modelID = ' num2str(modelID)]);
+paramcode = fetch(paramcode);
+paramcode = paramcode.Data{1};
+
+%Fetch each pair of nev files to run
+conn = database('','root','Fairbanks1!','com.mysql.jdbc.Driver', ...
+	'jdbc:mysql://fairbanks.amath.washington.edu:3306/spanky_db');
+toprocess = exec(conn, ['SELECT `manualrecording`, `1DBCrecording`, `manualrecordingafter` FROM experiment_tuning']);
+toprocess = fetch(toprocess);
+toprocess = toprocess.Data;
+toprocess = reshape(toprocess,1,[]);
+nR = size(toprocess,2);
+
+%rng('shuffle')
+for idx = 1:nR
+	nevfile = toprocess{idx};
+	display(['Processing ' nevfile])
+	if ~strcmp(nevfile, 'null')
+		processGrangerGLMShuffled(conn, modelID, blackrock, nevfile, paramcode);
+	end
+end
+
