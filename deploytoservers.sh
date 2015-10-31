e4ff78f9366520e5c53d5c42664b5016a690e545
Ben Lansdell <lansdell@uw.edu>
2015-10-31 12:34:08 -0700

    Update deploytoservers text

--- deploytoservers.sh
+++ deploytoservers.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-echo "Deploy to espresso"
+echo "=======Deploy to espresso========"
 git push espresso master
-echo "Deploy to mocha"
+echo "========Deploy to mocha========"
 git push mocha master
-echo "Deploy to galao"
+echo "========Deploy to galao========"
 git push galao master
-echo "Deploy to fairhall cylinder"
+echo "========Deploy to fairhall cylinder========"
 git push fairhall master
-echo "Deploy to latte"
+echo "========Deploy to latte========"
 git push latte master
 #git push hyak master
