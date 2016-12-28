0308c833c58c2cebc7392ec3b3d2179b67143cd9
Ben Lansdell <lansdell@uw.edu>
2016-12-28 11:52:39 -0800

    Remove a bunch of stuff

--- matlab/other/pillow_code_GLM_v1/GLMcode/nlfuns/expfun.m
+++
@@ -1,8 +0,0 @@
-function [f,df,ddf] = expfun(x)
-%  [f,df,ddf] = expfun(x)
-%
-%  replacement for 'exp' that returns 3 arguments (value, 1st & 2nd deriv)
-
-f = exp(x);
-df = f;
-ddf = df;
--- matlab/other/pillow_code_GLM_v1/setpaths.m
+++
@@ -1,9 +0,0 @@
-basedir = pwd;
-addpath([basedir '/tools_mexcode/']);
-addpath([basedir '/tools_splines/']);
-addpath([basedir '/tools_misc/']);
-addpath([basedir '/GLMcode/']);
-addpath([basedir '/GLMcode/nlfuns/']);
-addpath([basedir '/testscripts/']);
-
-global RefreshRate;  % Stimulus frame rate (Hz)
